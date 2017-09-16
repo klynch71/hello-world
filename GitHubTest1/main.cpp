6d26ce543ddf18336afc30076e2c814484d4cd7d
Kevin Lynch <klynch71@gmail.com>
2017-09-16 09:06:42 -0700

    Initial Commit

---
+++ GitHubTest1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitHubTest1
+//
+//  Created by Kevin Lynch on 9/16/17.
+//  Copyright © 2017 Kevin Lynch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
